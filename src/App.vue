0caa1fd3b0a65f712d52d8759671b8ec0c81d67c
Kir2410 <kkv2410@gmail.com>
2023-12-01 18:41:35 +0700

    Fixed app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Практика публикации на GitHub pages</h1>
 </template>
 
 <script lang="ts">
@@ -10,7 +9,6 @@ import HelloWorld from './components/HelloWorld.vue';
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 });
 </script>
